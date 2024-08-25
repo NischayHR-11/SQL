ce2b03d1802c2d9078f7cda8d3ccafa0bc7d504b
NISCHAY HR <nischayhr11@gmail.com>
2024-08-26 05:06:06 +0530

    Modified data type of id

---
+++ REST/schema.sql
@@ -0,0 +1,11 @@
+-- create table users(
+
+--     id varchar(100) primary key,
+--     name varchar(50) not null ,
+--     email varchar(50)  unique not null,
+--     password varchar(50) not null
+-- );
+
+
+alter table users
+modify id varchar(100);
